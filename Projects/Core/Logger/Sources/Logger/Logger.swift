f00db70ebb3ebbe3171f5edf06b9c776e554fa88
gnoes-ios <park_juseong@naver.com>
2025-12-08 17:44:34 +0900

    feat: Logger 구현

---
+++ Projects/Core/Logger/Sources/Logger/Logger.swift
@@ -0,0 +1,37 @@
+import OSLog
+
+public final class Logger {
+    public static let shared = Logger(configuration: .default)
+    
+    private let osLogger: OSLog
+    private let configuration: LoggerConfiguration
+    
+    public init(configuration: LoggerConfiguration) {
+        self.configuration = configuration
+        self.osLogger = OSLog(
+            subsystem: configuration.subsystem,
+            category: configuration.category
+        )
+    }
+    
+    func log(
+        _ message: String,
+        level: LogLevel = .info,
+        category: LogCategory = .general,
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line
+    ) {        
+        #if DEBUG
+        let fileName = (file as NSString).lastPathComponent
+        let logMessage = "[\(category.rawValue)] [\(fileName):\(line)] \(function) - \(message)"
+        
+        os_log(
+            level.osLogType,
+            log: osLogger,
+            "%{public}@",
+            logMessage
+        )
+        #endif
+    }
+}
