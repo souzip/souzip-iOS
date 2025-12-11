5bdebefdb837ad5fa95d1d6fdaa95e99897b35c9
gnoes-ios <park_juseong@naver.com>
2025-12-12 00:07:42 +0900

    refactor: LogAction 파라미터 수정

--- Projects/App/Sources/Coordinator/AppRoute.swift
+++ Projects/App/Sources/Coordinator/AppRoute.swift
@@ -1,13 +1,5 @@
-enum AppRoute: CustomStringConvertible {
+enum AppRoute {
     case splash
     case auth
     case main
-
-    var description: String {
-        switch self {
-        case .splash: "Splash 화면 이동"
-        case .auth: "Auth 화면 이동"
-        case .main: "Main 화면 이동"
-        }
-    }
 }
