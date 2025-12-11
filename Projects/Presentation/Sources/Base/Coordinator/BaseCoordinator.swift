c6bda45d3b4685022bf4d51051bb9fef789201b8
gnoes-ios <park_juseong@naver.com>
2025-12-12 00:02:26 +0900

    feat: Base Coordinator 구현

---
+++ Projects/Presentation/Sources/Base/Coordinator/BaseCoordinator.swift
@@ -0,0 +1,21 @@
+import Logger
+import UIKit
+
+@MainActor
+open class BaseCoordinator<Route>: Coordinator {
+    public let nav: UINavigationController
+    public var children: [any Coordinator] = []
+    public weak var parent: (any Coordinator)?
+
+    public init(nav: UINavigationController) {
+        self.nav = nav
+        Logger.shared.logLifecycle()
+    }
+
+    deinit {
+        Logger.shared.logLifecycle()
+    }
+
+    open func start() {}
+    open func navigate(_ route: Route) {}
+}
