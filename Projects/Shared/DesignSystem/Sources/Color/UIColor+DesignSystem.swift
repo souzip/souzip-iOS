21e73626bdbefaea397a89b2c1e8e5ffe984ac9e
gnoes-ios <park_juseong@naver.com>
2025-12-16 20:50:40 +0900

    feat: DesignSystem 컬러 추가

---
+++ Projects/Shared/DesignSystem/Sources/Color/UIColor+DesignSystem.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+public extension UIColor {
+    // MARK: - Grey
+
+    static let dsGreyWhite: UIColor = .greyWhite
+    static let dsGrey80: UIColor = .grey80
+    static let dsGrey300: UIColor = .grey300
+    static let dsGrey500: UIColor = .grey500
+    static let dsGrey700: UIColor = .grey700
+    static let dsGrey800: UIColor = .grey800
+    static let dsGrey900: UIColor = .grey900
+
+    // MARK: - Main
+
+    static let dsMainR50: UIColor = .mainR50
+    static let dsMainR100: UIColor = .mainR100
+    static let dsMain: UIColor = .main
+
+    // MARK: - Secondary
+
+    static let dsSecondaryYellow: UIColor = .secondaryYellow
+    static let dsSecondaryGreen: UIColor = .secondaryGreen
+    static let dsSecondaryPurple: UIColor = .secondaryPurple
+    static let dsSecondaryBlue: UIColor = .secondaryBlue
+}
