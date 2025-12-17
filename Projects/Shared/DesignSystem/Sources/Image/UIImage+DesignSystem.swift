d14e3c204009c50ca897d303e35853c4279d7ab0
gnoes-ios <park_juseong@naver.com>
2025-12-17 19:31:51 +0900

    design: 로그인 UI 관련 이미지 추가

---
+++ Projects/Shared/DesignSystem/Sources/Image/UIImage+DesignSystem.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+public extension UIImage {
+    // MARK: - Logo
+
+    static let dsLogoSouzip: UIImage = .logoSouzip
+
+    static let dsLogoKakao: UIImage = .logoKakao
+    static let dsLogoGoogle: UIImage = .logoGoogle
+    static let dsLogoApple: UIImage = .logoApple
+
+    // MARK: - Image
+
+    static let dsLoginBackground: UIImage = .loginBackground
+    static let dsLoginRecent: UIImage = .loginRecent
+}
