ca4b1b18eb4db69d6a37aac7aece71e5120cf3df
gnoes-ios <park_juseong@naver.com>
2025-12-22 23:55:57 +0900

    feat:(DS): Profile 이미지 추가

---
+++ Projects/Shared/DesignSystem/Sources/Image/DSImage.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+public extension UIImage {
+    // MARK: - Login
+
+    static let dsLoginBackground: UIImage = .loginBackground
+    static let dsLoginRecent: UIImage = .loginRecent
+
+    // MARK: - Profile
+
+    static let dsProfileDefault: UIImage = .dsProfileDefault
+    static let dsProfileRed: UIImage = .dsProfileRed
+    static let dsProfileYellow: UIImage = .dsProfileYellow
+    static let dsProfilePurple: UIImage = .dsProfilePurple
+    static let dsProfileBlue: UIImage = .dsProfileBlue
+}
