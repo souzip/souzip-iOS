56f9b9d34db3494152097badcdc02cf15eaa9acc
gnoes-ios <park_juseong@naver.com>
2026-01-23 02:08:59 +0900

    feat: AdMob 모듈 구성

---
+++ Projects/Core/AdMob/Sources/Base/AdMobManager.swift
@@ -0,0 +1,12 @@
+import GoogleMobileAds
+
+public final class AdMobManager {
+
+    public static let shared = AdMobManager()
+
+    private init() {}
+
+    public func initialize() {
+        MobileAds.shared.start(completionHandler: nil)
+    }
+}
--- Projects/Shared/Utils/Sources/App/InfoPlistKey.swift
+++ Projects/Shared/Utils/Sources/App/InfoPlistKey.swift
@@ -9,4 +9,5 @@ public enum InfoPlistKey: String {
     case kakaoAppKey = "KAKAO_APP_KEY"
     case googleClientID = "GOOGLE_CLIENT_ID"
     case amplitudeAPIKey = "AMPLITUDE_API_KEY"
+    case bannerUnitID = "ADMOB_BANNER_AD_UNIT_ID"
 }
