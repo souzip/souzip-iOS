509c365003e9f468193e94607c0687a8ebf8a932
gnoes-ios <park_juseong@naver.com>
2026-02-02 17:45:34 +0900

    refactor: TemrsItem 구조 정리

--- Projects/Domain/Sources/Onboarding/Model/Terms/TermsItem.swift
+++ Projects/Domain/Sources/Onboarding/Model/Terms/TermsItem.swift
@@ -1,22 +1,19 @@
 public struct TermsItem: Hashable {
     public let type: TermsType
-    public let isRequired: Bool
     public var isAgreed: Bool
 
     public init(
         type: TermsType,
-        isRequired: Bool,
         isAgreed: Bool = false
     ) {
         self.type = type
-        self.isRequired = isRequired
         self.isAgreed = isAgreed
     }
 }
 
 public extension [TermsItem] {
     var isRequiredAllAgreed: Bool {
-        filter(\.isRequired)
+        filter(\.type.isRequired)
             .allSatisfy(\.isAgreed)
     }
 
