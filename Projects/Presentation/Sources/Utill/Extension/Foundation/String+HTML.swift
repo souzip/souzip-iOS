78553e1db9fcb5ed46db066551480856c157744b
gnoes-ios <park_juseong@naver.com>
2026-03-18 00:04:38 +0900

    refactor: strippedHTML에 @MainActor 적용으로 메인 스레드 호출 계약 강제

--- Projects/Presentation/Sources/Utill/Extension/Foundation/String+HTML.swift
+++ Projects/Presentation/Sources/Utill/Extension/Foundation/String+HTML.swift
@@ -2,7 +2,7 @@ import UIKit
 
 extension String {
     // HTML 태그 및 엔티티를 제거한 순수 텍스트 반환
-    // NSAttributedString HTML 파싱을 사용하므로 메인 스레드에서 호출 필요
+    @MainActor
     var strippedHTML: String {
         guard let data = data(using: .utf8),
               let attributed = try? NSAttributedString(
