4e5c07509d39ac4d3d94188273c949d61c188857
gnoes-ios <park_juseong@naver.com>
2026-03-23 18:17:46 +0900

    chore: Tuist Core 모듈 그래프 및 패키지 의존성 정리

--- Tuist/ProjectDescriptionHelpers/Dependencies/ModuleDependencies.swift
+++ Tuist/ProjectDescriptionHelpers/Dependencies/ModuleDependencies.swift
@@ -8,81 +8,82 @@ public enum ModuleDependencies {
                 .module(.presentation),
                 .module(.domain),
                 .module(.data),
+                .module(.designSystem),
+                .module(.analytics),
+                .module(.storage),
+                .module(.networking),
+                .module(.utils),
             ]
-            
+
         case .presentation:
             return [
                 .module(.domain),
                 .module(.logger),
+                .module(.analytics),
                 .module(.designSystem),
                 .module(.utils),
-                .module(.adMob),
+                .module(.ads),
 
                 .external(.rxSwift),
                 .external(.rxRelay),
                 .external(.rxCocoa),
                 .external(.kingfisher),
-                .external(.mapboxMaps)
+                .external(.mapboxMaps),
             ]
-            
+
         case .domain:
-            return [
-                .module(.utils),
-                .module(.logger)
-            ]
-            
+            return []
+
         case .data:
             return [
                 .module(.domain),
                 .module(.networking),
                 .module(.logger),
-                .module(.keychain),
-                .module(.userDefaults),
+                .module(.storage),
                 .module(.utils),
+                .module(.analytics),
 
                 .external(.kakaoSDKCommon),
                 .external(.kakaoSDKAuth),
                 .external(.kakaoSDKUser),
                 .external(.googleSignIn),
             ]
-            
+
         case .networking:
             return [
-                .module(.logger)
+                .module(.logger),
             ]
 
         case .logger:
+            return []
+
+        case .analytics:
             return [
-                .external(.amplitudeSwift)
+                .external(.amplitudeSwift),
             ]
 
-        case .keychain:
-            return [
-                .module(.logger),
-                .module(.utils)
-            ]
-
-        case .userDefaults:
-            return [
-                .module(.logger),
-                .module(.utils)
-            ]
-            
-        case .adMob:
+        case .storage:
             return [
                 .module(.logger),
                 .module(.utils),
-                
-                .external(.googleMobileAds)
+            ]
+
+        case .ads:
+            return [
+                .module(.logger),
+                .module(.analytics),
+                .module(.utils),
+
+                .external(.googleMobileAds),
             ]
 
         case .designSystem:
             return [
                 .module(.logger),
                 .module(.utils),
-                .external(.snapKit)
+                .external(.snapKit),
             ]
-            
+
         case .utils:
             return []
         }
