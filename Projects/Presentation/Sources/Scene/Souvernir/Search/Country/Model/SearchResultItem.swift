9e3f563fb52ead906b0481c0ab30a2ecf714a824
gnoes-ios <park_juseong@naver.com>
2026-04-01 20:16:19 +0900

    refactor: SearchResultItem 모델 place 타입 대응

--- Projects/Presentation/Sources/Scene/Souvernir/Search/Country/Model/SearchResultItem.swift
+++ Projects/Presentation/Sources/Scene/Souvernir/Search/Country/Model/SearchResultItem.swift
@@ -1,8 +1,8 @@
 import CoreLocation
 
 enum SearchResultType {
-    case country
     case city
+    case place
 }
 
 struct SearchResultItem: Hashable {
@@ -10,7 +10,27 @@ struct SearchResultItem: Hashable {
     let name: String
     let subName: String
     let type: SearchResultType
+    let placeCategory: String
+    let placeRegion: String
     let coordinate: CLLocationCoordinate2D
+
+    init(
+        id: String,
+        name: String,
+        subName: String = "",
+        type: SearchResultType,
+        placeCategory: String = "",
+        placeRegion: String = "",
+        coordinate: CLLocationCoordinate2D
+    ) {
+        self.id = id
+        self.name = name
+        self.subName = subName
+        self.type = type
+        self.placeCategory = placeCategory
+        self.placeRegion = placeRegion
+        self.coordinate = coordinate
+    }
 
     static func == (lhs: SearchResultItem, rhs: SearchResultItem) -> Bool {
         lhs.id == rhs.id
