4c81c3c2b05b5bc8aa68275da34bbfdb2af99639
gnoes-ios <park_juseong@naver.com>
2026-04-08 03:22:41 +0900

    chore: 빌드번호 증가

--- Tuist/ProjectDescriptionHelpers/Core/Environment.swift
+++ Tuist/ProjectDescriptionHelpers/Core/Environment.swift
@@ -11,7 +11,7 @@ public enum Environment {
     // MARK: - Version
 
     public static let appVersion = "1.2.0"
-    public static let appBuildVersion = "2"
+    public static let appBuildVersion = "3"
     public static let swiftVersion = "5.9"
 
     // MARK: - Platform
